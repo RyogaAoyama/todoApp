d592521021f65b270a14918264cf8fda2f49c2a5
RyogaAoyama <ryogaaoym0114@gmail.com>
2019-12-09 19:29:12 +0900

    rubocopで整形

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   namespace :api, format: 'json' do
-    resources :todos, only: [:index,:create, :destroy, :update]
+    resources :todos, only: %i[index create destroy update]
   end
   root to: 'homes#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
